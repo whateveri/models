68ba679fa04a986c9e18db7816a33343a306e088
whateveri <47405486+whateveri@users.noreply.github.com>
2025-06-25 19:17:18 +0200

    Update eigine.py
    add torch. before nn

--- generalModel/eigine.py
+++ generalModel/eigine.py
@@ -3,9 +3,9 @@ from tqdm.auto import tqdm
 from typing import List,Dict,Tuple
 
 
-def train_step(model:nn.Module,
+def train_step(model:torch.nn.Module,
                dataloader:torch.utils.data.DataLoader,
-               loss_fn:nn.Module,
+               loss_fn:torch.nn.Module,
                optimizer:torch.optim.Optimizer,
                device:torch.device)-> Tuple[float,float]:
     model.train()
